b136627f0830e581af87b01e1570a577131c40d6
Yoggi-n <yogeswaran.nn@outlook.com>
2022-08-22 12:58:25 +0530

    Added cookie session middleware and api routers

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors, { CorsOptions } from 'cors';
+import { API } from './api';
+import cookieSession from 'cookie-session';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 3000;
@@ -12,8 +14,15 @@ const corsOptions: CorsOptions = {
     credentials: true,
 }
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieSession({
+    name: 'session',
+    keys: [process.env.COOKIE_SECRET!],
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production' || false,
+}));
+app.use(bodyParser.json());
 app.use(cors(corsOptions));
+app.use('/api', API);
 
 app.listen(PORT, () => {
     console.log(`App listening at port: ${PORT}`);
