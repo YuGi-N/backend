ec78d03f430bc080eb4b5804535003b114f4fe2f
Yoggi-n <yogeswaran.nn@outlook.com>
2022-08-22 17:33:34 +0530

    Added authmiddleware for movie url paths

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import authMiddleware from '../middlewares/authMiddleware';
 import { MovieRouter } from './movie';
 import { UserRouter } from './user';
 
 const router = Router();
 
-router.use('/movie', MovieRouter);
+router.use('/movie', authMiddleware, MovieRouter);
 router.use('/user', UserRouter);
 
 export { router as API };
