c17a51e1371220f2847f92d283866d7ad558c44a
Yoggi-n <yogeswaran.nn@outlook.com>
2022-08-22 21:03:45 +0530

    Modifier cors

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const corsOptions: CorsOptions = {
-    origin: '*',
+    origin: process.env.CLIENT_URL,
     allowedHeaders: ['Content-Type'],
     credentials: true,
 }
